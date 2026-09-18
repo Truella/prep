b5e34701bd5987876ac5b196701e9683527948b1
truella <turahsuleiman1212@gmail.com>
2026-09-18 19:20:32 +0100

    feat: comment out Problem component in Home page

--- app/page.tsx
+++ app/page.tsx
@@ -18,7 +18,7 @@ export default function Home() {
 		>
 			<ExternalNav />
 			<Hero />
-			<Problem />
+			{/* <Problem /> */}
 			<HowItWorks />
 			<CBTExperienceSection />
 			<AIReviewSection />
